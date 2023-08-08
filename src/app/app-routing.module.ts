6be2d74bd69a3c7b79ac510b7476caba50ec73e6
kcrawfordtaylor <kcrawfordtaylor@gmail.com>
2023-08-08 15:33:29 -0500

    Defaults to homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 const routes: Routes = [
 {path: 'fibonacci', component: FibonacciComponent},
 {path: 'home', component: DashboardComponent},
+{path: '', component: DashboardComponent},
 {path: 'reversed-int', component: ReverseIntComponent}
 ];
 
