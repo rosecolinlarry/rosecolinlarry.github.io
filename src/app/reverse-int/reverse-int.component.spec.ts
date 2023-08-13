b5a7c74ecb9c5749c3bcc3e7995fb73407fbf517
kcrawfordtaylor <kcrawfordtaylor@gmail.com>
2023-08-13 09:29:18 -0500

    rebuilt to docs

--- src/app/reverse-int/reverse-int.component.spec.ts
+++ src/app/reverse-int/reverse-int.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ReverseIntComponent } from './reverse-int.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ReverseIntComponent', () => {
   let component: ReverseIntComponent;
@@ -8,7 +12,8 @@ describe('ReverseIntComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ReverseIntComponent]
+      declarations: [ReverseIntComponent],
+      imports: [MatFormFieldModule, ReactiveFormsModule, MatInputModule, BrowserAnimationsModule]
     });
     fixture = TestBed.createComponent(ReverseIntComponent);
     component = fixture.componentInstance;
