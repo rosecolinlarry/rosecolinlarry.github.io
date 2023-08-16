206933af4352145a9497a233f51e54e85e6c80d3
kcrawfordtaylor <kcrawfordtaylor@gmail.com>
2023-08-15 21:29:46 -0500

    Uses ngFor to load dashboard buttons

--- src/app/shared/dashboard/dashboard.component.ts
+++ src/app/shared/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { LinkData } from '../Interfaces/link-data';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  @Input() linkData: LinkData[] = []
 
+  constructor() {
+    this.linkData = [
+      {url: '/coding-exercises/fibonacci', name: 'Fibonacci'},
+      {url: '/coding-exercises/reversed-int', name: 'Reversed Integer'},
+      {url: '/fantasy-shop', name: 'Fantasy Shop'},
+    ]
+  }
 }
