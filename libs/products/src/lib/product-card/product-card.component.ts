d9bbc8ef414d2b05976cae3e45cdb716f9a93339
Kaisa <keaackerman@gmail.com>
2024-04-21 15:53:32 -0500

    added dropdown menu on product cards

--- libs/products/src/lib/product-card/product-card.component.ts
+++ libs/products/src/lib/product-card/product-card.component.ts
@@ -3,22 +3,24 @@ import { CommonModule, CurrencyPipe, PercentPipe } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { CondensedStarRatingComponent } from './star-rating/condensed-star-rating/condensed-star-rating.component';
 
 @Component({
-    selector: 'lib-product-card',
-    standalone: true,
-    templateUrl: './product-card.component.html',
-    styleUrl: './product-card.component.scss',
-    imports: [
-        CommonModule,
-        MatButtonModule,
-        MatCardModule,
-        MatIconModule,
-        CondensedStarRatingComponent,
-        CurrencyPipe,
-        PercentPipe
-    ]
+  selector: 'lib-product-card',
+  standalone: true,
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.scss',
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    CondensedStarRatingComponent,
+    CurrencyPipe,
+    PercentPipe,
+    MatMenuModule
+  ]
 })
 export class ProductCardComponent {
 
@@ -30,18 +32,22 @@ export class ProductCardComponent {
   averageStars = 4.83;
   @Input()
   sellerName = "A Real Shop";
-@Input()
-salePrice = 29.99;
-@Input()
-originalPrice = 69.99;
-@Input()
-verified = true;
-@Input()
-get percentOff(): number{
-return this.onSale && this.originalPrice && this.salePrice ? this.salePrice/this.originalPrice  : 0;
-}
-@Input()
-onSale = true;
-@Input()
-freeShipping = true;
+  @Input()
+  salePrice = 29.99;
+  @Input()
+  originalPrice = 69.99;
+  @Input()
+  verified = true;
+  @Input()
+  isFavorite = false;
+  @Input()
+  imageSrc = "https://i.imgur.com/is4kHp1.jpeg";
+  @Input()
+  get percentOff(): number {
+    return this.onSale && this.originalPrice && this.salePrice ? this.salePrice / this.originalPrice : 0;
+  }
+  @Input()
+  onSale = true;
+  @Input()
+  freeShipping = true;
 }
