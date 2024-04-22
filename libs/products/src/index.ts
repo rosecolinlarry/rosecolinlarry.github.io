ade8ab94425310bfa44d6adf1c08614b26564a9a
Kaisa <keaackerman@gmail.com>
2024-04-21 19:54:18 -0500

    Cleaning up the shop (#11)
    * moved shop item to the library
    
    so far the green is still there
    
    * added 404 page
    
    added storefront component
    
    * added basic dashboard and mini cards
    
    copied from here: https://www.smashingmagazine.com/2020/07/responsive-dashboard-angular-material-ng2-charts-schematics/
    
    and here: https://github.com/adudecoder/angular-store-admin-ecommerce/blob/main/src/app/services/sales/sales.service.ts
    
    going to now strip out their code and put mine in
    
    * added apex
    
    * got a nice grid list going
    
    * added hover color in sidenav
    
    * formatted the product cards just how i want them
    
    used a ridiculous amount of inline styling. will clean up into classes. it was faster to prototype this way though
    
    * added dropdown menu on product cards
    
    * added fake data
    
    * handles word wrapping
    
    * rebuilt docs

--- libs/products/src/index.ts
+++ libs/products/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/product-card/product-card.component';
+export * from './lib/product-card/product-card-info.interface';
 export * from './lib/product-card/star-rating/full-star-rating/full-star-rating.component';
 export * from './lib/product-card/star-rating/condensed-star-rating/condensed-star-rating.component';
