5c2f2c363e445c7bf07e6fb81f59b2e09ed78a65
Kaisa <keaackerman@gmail.com>
2024-07-28 22:44:47 -0500

    added new site
    it now servers

--- libs/products/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/product-card/product-card.component';
-export * from './lib/product-card/product-card-info.interface';
-export * from './lib/product-card/star-rating/full-star-rating/full-star-rating.component';
-export * from './lib/product-card/star-rating/condensed-star-rating/condensed-star-rating.component';
